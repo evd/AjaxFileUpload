9eae8c4d9a83791107695f03ceb0cb9cb9d742a8
evd <toevgd@gmail.com>
2012-11-17 23:30:07 +0400

    Fix ajax errors on non authorized users

--- assets/components/ajaxfileupload/upload.php
+++ assets/components/ajaxfileupload/upload.php
@@ -7,7 +7,7 @@
 DEFINE('MODX_API_MODE', true);
 require_once dirname(dirname(dirname(dirname(__FILE__)))).'/config.core.php';
 require_once MODX_CORE_PATH.'config/'.MODX_CONFIG_KEY.'.inc.php';
-require_once MODX_CONNECTORS_PATH.'index.php';
+require_once MODX_BASE_PATH.'index.php';
 
 $processorsPath = $modx->getOption('ajaxfileupload.core_path',null,$modx->getOption('core_path').'components/ajaxfileupload/').'processors/';
 
